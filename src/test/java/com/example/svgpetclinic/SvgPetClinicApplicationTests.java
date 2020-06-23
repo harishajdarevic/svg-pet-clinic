c20965948fa3a7d26b1fd509f50f367bbb2e100a
haris Hajdarevic <haris.hajdarevic@innovic.io>
2020-06-23 22:33:18 +0200

    Initial

---
+++ src/test/java/com/example/svgpetclinic/SvgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.svgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
